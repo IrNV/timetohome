91876f5ef8ca1a1ed7c47750bcbf6db2244ccb5a
Valera <NsT.valerka@ukr.net>
2018-09-22 20:16:29 +0300

    new project

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')30azrdx#rb6v7q+*@*xu0lzgb1$9as!dt)#)eo!y%4_h*ff&6'
+SECRET_KEY = 'kvke7=pu1mh$19*fga-r0_!ze&w6=ku%n^f=(9_eb*c7#ddek8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'timetohome',
+    'timetracker',
 ]
 
 MIDDLEWARE = [
--- timetracker/models.py
+++ timetracker/models.py
@@ -3,7 +3,8 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    direction = models.CharField(max_length=200)
     time = models.TimeField()
     created_date = models.DateTimeField(
             default=timezone.now)
@@ -15,4 +16,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.direction
