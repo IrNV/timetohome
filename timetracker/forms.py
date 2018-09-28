4ac31ffad270b0eaf9fca87368900c979d5a095f
Valera <NsT.valerka@ukr.net>
2018-09-28 21:11:45 +0300

    add delete func

--- timetracker/forms.py
+++ timetracker/forms.py
@@ -5,7 +5,7 @@ from .models import Post
 class PostForm(forms.ModelForm):
     direction = forms.ChoiceField(choices=[("From home to university", "From home to university"),
     	("From University to home", "From University to home")])
-    time = forms.TimeField()
+    time = forms.TimeField(widget=forms.TimeInput(format=('hh-mm-ss')))
     class Meta:
         model = Post
         fields = ('direction', 'time')
--- timetracker/views.py
+++ timetracker/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'timetracker/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -38,3 +38,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'timetracker/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
