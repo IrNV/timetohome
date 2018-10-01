4f1de550ff9ed0c7a385ae9f35ee34ec99ad0803
Valera <NsT.valerka@ukr.net>
2018-10-01 23:11:02 +0300

    add some fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'kvke7=pu1mh$19*fga-r0_!ze&w6=ku%n^f=(9_eb*c7#ddek8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'django-timetracker.herokuapp.com']
 
 
 # Application definition
